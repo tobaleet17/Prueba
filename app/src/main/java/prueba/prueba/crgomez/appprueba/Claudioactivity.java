d2f091a2db0b0f5cde43690ae3b696ff44a5c161
Cristobal Gomez Mora <crgomo01@floridauniversitaria.es>
2018-09-25 18:35:19 +0200

    Initial commit

---
+++ app/src/main/java/prueba/prueba/crgomez/appprueba/Claudioactivity.java
@@ -0,0 +1,13 @@
+package prueba.prueba.crgomez.appprueba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Claudioactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_claudioactivity);
+    }
+}
